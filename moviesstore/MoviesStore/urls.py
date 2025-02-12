eb88b56ccf05032eafc3ad4bd29c29789a01d0d6
munchinoncreatine <jjameskiim@gmail.com>
2025-02-12 00:09:03 -0500

    first commit

--- moviesstore/MoviesStore/urls.py
+++ moviesstore/MoviesStore/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('movies/', include('movies.urls')),
 ]
 
 # this is a comment to test git
