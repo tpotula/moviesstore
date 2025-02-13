403b5fa26f2c441ebe6d5a2904738b29f478540f
munchinoncreatine <jjameskiim@gmail.com>
2025-02-13 00:20:27 -0500

    chapter 10: shopping cart add to shopping cart remove all movies from shopping cart

--- moviesstore/MoviesStore/urls.py
+++ moviesstore/MoviesStore/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('movies/', include('movies.urls')),
 
     path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT)
